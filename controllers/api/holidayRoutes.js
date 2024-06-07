81e404e32e85b21146501de2df37ffed6d992a52
Tom Bee <tburzynski2@yahoo.com>
2024-06-06 21:33:38 -0500

    API routes for movies & skeleton file for holidays

---
+++ controllers/api/holidayRoutes.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const { Holiday } = require("../../models");
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,10 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const movieRoutes = require("./movieRoutes");
+// const holidayRoutes = require("./holidayRoutes");
 
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
+router.use("/movies", movieRoutes);
+// router.use("/holidays", holidayRoutes);
 
 module.exports = router;
