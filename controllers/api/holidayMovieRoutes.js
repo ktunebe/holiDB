25a961c6c8f1189e08a944c4d06b86a9e2d67c9e
Soko77788 <NickSokolowski7@gmail.com>
2024-06-13 14:19:27 -0500

    tags button and delete routes working for adding holiday to movie

--- controllers/api/holidayMovieRoutes.js
+++ controllers/api/holidayMovieRoutes.js
@@ -13,13 +13,15 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:movie_id/:holiday_id", async (req, res) => {
   try {
     const holidayMovieData = await HolidayMovie.destroy({
       where: {
-        id: req.params.id,
+        movie_id: req.params.movie_id,
+        holiday_id: req.params.holiday_id,
       },
     });
+
 
     if (!holidayMovieData) {
       res.status(404).json({
