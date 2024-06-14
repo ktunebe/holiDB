d407e8fc867eb88922d435809ce31fa7ee46e268
ktunebe <ktunebe@gmail.com>
2024-06-13 21:10:50 -0500

    Changed connection.js as indicated on slack

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'postgres',
+  ? new Sequelize(process.env.DB_URL, {
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false
+      }
     }
-  );
+  })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+      }
+    );
 
 module.exports = sequelize;
